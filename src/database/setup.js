55abf27e02bf66107013986b4db4e355bdc43f70
Alex Maldonado <emtamaldonado@gmail.com>
2021-05-17 12:11:33 -0700

    Remove ATLAS connection string from .env file for RA instructor eval

--- src/database/setup.js
+++ src/database/setup.js
@@ -1,43 +1,66 @@
 const mongoose = require('mongoose');
 
-if (process.env.NODE_ENV === 'development') {
-  // LOCAL DATABASE
-  module.exports = function() {
-    mongoose
-      .connect(process.env.DATABASE_LOCAL, {
+//ATLAS CONNECTION
+module.exports = function() {
+  mongoose
+    .connect(
+      'mongodb+srv://admin-alex:test123@hotsaucecluster.aupxg.mongodb.net/eventsAppCloud',
+      {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
-      })
-      .then(() => {
-        // eslint-disable-next-line no-console
-        console.log('LOCAL MONGO CONNECTION OPEN!!!');
-      })
-      .catch((err) => {
-        // eslint-disable-next-line no-console
-        console.log('LOCAL MONGO CONNECTION ERROR!!!!');
-        // eslint-disable-next-line no-console
-        console.log(err);
-      });
-  };
-} else {
-  //ATLAS CLOUD DATABASE
-  module.exports = function() {
-    mongoose
-      .connect(process.env.DATABASE_CLOUD, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      })
-      .then(() => {
-        // eslint-disable-next-line no-console
-        console.log('ATLAS MONGO CONNECTION OPEN!!!');
-      })
-      .catch((err) => {
-        // eslint-disable-next-line no-console
-        console.log('ATLAS MONGO CONNECTION ERROR!!!!');
-        // eslint-disable-next-line no-console
-        console.log(err);
-      });
-  };
-}
+      }
+    )
+    .then(() => {
+      // eslint-disable-next-line no-console
+      console.log('ATLAS MONGO CONNECTION OPEN!!!');
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log('ATLAS MONGO CONNECTION ERROR!!!!');
+      // eslint-disable-next-line no-console
+      console.log(err);
+    });
+};
+
+// if (process.env.NODE_ENV === 'development') {
+//   // LOCAL DATABASE
+//   module.exports = function() {
+//     mongoose
+//       .connect(process.env.DATABASE_LOCAL, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: false,
+//       })
+//       .then(() => {
+//         // eslint-disable-next-line no-console
+//         console.log('LOCAL MONGO CONNECTION OPEN!!!');
+//       })
+//       .catch((err) => {
+//         // eslint-disable-next-line no-console
+//         console.log('LOCAL MONGO CONNECTION ERROR!!!!');
+//         // eslint-disable-next-line no-console
+//         console.log(err);
+//       });
+//   };
+// } else {
+//   //ATLAS CLOUD DATABASE
+//   module.exports = function() {
+//     mongoose
+//       .connect(process.env.DATABASE_CLOUD, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: false,
+//       })
+//       .then(() => {
+//         // eslint-disable-next-line no-console
+//         console.log('ATLAS MONGO CONNECTION OPEN!!!');
+//       })
+//       .catch((err) => {
+//         // eslint-disable-next-line no-console
+//         console.log('ATLAS MONGO CONNECTION ERROR!!!!');
+//         // eslint-disable-next-line no-console
+//         console.log(err);
+//       });
+//   };
+// }
